ae0e2e08c38c03b3c1cae71f1629870aaebf03c2
Mahdi <mahditahavorgar@gmail.com>
2020-10-18 10:30:57 +0330

    ADD: Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,55 +1,65 @@
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   
   siteMetadata: {
-    title: `Mahdi | Front-end Developer`,
-    description: `I’m Mahdi, a 26 years old self-taught Front-end developer`,
-    author: `Mahdi`,
-    siteUrl: 'https://Mahdi.dev',
-    social: {
-      twitter: '@Thg_code',
-      instagram: 'code_thg',
-      linkedin: 'mahdi-tahavorgar',
-      facebook: 'Mahdi.Thgr',
-      github: 'MahdiiiThg',
-      email: 'mahditahavorgar@gmail.com'
-    },
-  },
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`,
-    
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`
+      title: `Mahdi | Front-end Developer`,
+      description: `I’m Mahdi, a 26 years old self-taught Front-end developer`,
+      author: `Mahdi`,
+      siteUrl: 'https://Mahdi.dev',
+      social: {
+        twitter: '@Thg_code',
+        instagram: 'code_thg',
+        linkedin: 'mahdi-tahavorgar',
+        facebook: 'Mahdi.Thgr',
+        github: 'MahdiiiThg',
+        email: 'mahditahavorgar@gmail.com'
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-          plugins: [
-              {
-                  resolve: `gatsby-remark-images`,
-                  options: {
-                      maxWidth: 590,
-                      linkImagesToOriginal: false,
-                  },
-              },
-          ],
+    plugins: [
+      `gatsby-plugin-sass`,
+      `gatsby-plugin-react-helmet`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-styled-components`,
+      
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/assets/images`
+        }
       },
-  },
-    
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `posts`,
+          path: `${__dirname}/src/posts`
+        },
+      },
+      {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+            plugins: [
+                {
+                    resolve: `gatsby-remark-images`,
+                    options: {
+                        maxWidth: 590,
+                        linkImagesToOriginal: false,
+                    },
+                },
+            ],
+        },
+      },
+      {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GA_TRACKING_ID,
+      },
+    },
   ],
 }
